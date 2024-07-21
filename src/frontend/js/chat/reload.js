efe90feef30f42cce2209279cc77a533ec4f38d7
Robby_Blue <69482496+Robby-Blue@users.noreply.github.com>
2024-07-22 01:34:56 +0200

    fix messages while reload

--- src/frontend/js/chat/reload.js
+++ src/frontend/js/chat/reload.js
@@ -1,16 +1,36 @@
-const ws = new WebSocket("ws://"+location.host+"/ws"+location.pathname);
+let isLoading = false
+let gotRequestWhileLoading = false
 
-ws.addEventListener("message", (event) => {
+function reloadChat() {
     const messagesDiv = document.getElementById("messages-container");
     const url = new URL(window.location.href);
     url.searchParams.set("messages_only", "True");
     fetch(url.toString())
-	.then(response => {
-	    return response.text();
-	})
-	.then(html => {
-	    messagesDiv.outerHTML = html;
+	    .then(response => {
+	        return response.text();
+	    })
+	    .then(html => {
+	        messagesDiv.outerHTML = html;
             const newMessagesDiv = document.getElementById("messages-container");
             newMessagesDiv.scrollTop = newMessagesDiv.scrollHeight;
+            
+            isLoading = false;
+            if (gotRequestWhileLoading) {
+                gotRequestWhileLoading = false
+                console.log("reload again")
+                reloadChat()
+            }
         });
+}
+
+const ws = new WebSocket("ws://"+location.host+"/ws"+location.pathname);
+
+ws.addEventListener("message", (event) => {
+    if(isLoading) {
+        gotRequestWhileLoading = true
+        console.log("reload saved")
+        return;
+    }
+    isLoading = true;
+    reloadChat();
 });
